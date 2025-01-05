01751f2ffd9b8ffe4657dfca83101597cec85ce2
Mark H <mark@newswift.dev>
2025-01-05 14:23:57 -0600

    Initial Commit

---
+++ UltimatePortfolioApp/UltimatePortfolioAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePortfolioAppApp.swift
+//  UltimatePortfolioApp
+//
+//  Created by Mark Honomichl on 1/5/25.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
